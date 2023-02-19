7004fac816627e4673d82eab8fe691d670dbbe85
Jehanzeb Siddiqui <100095101+Jehanzeb11@users.noreply.github.com>
2023-02-19 17:11:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const app = express()
+const AuthRoute = require("./routes/UserRoute")
+
+
+dotenv.config()
+
+app.use(express.json())
+
+
+const port = process.env.port || 5500
+
+
+
+app.use("/api/auth",AuthRoute)
+
+
+
+
+
+
+
+
+mongoose.connect(process.env.mongoUri).then(()=>{
+    console.log('mongoDb connected')
+}).catch((err)=>{
+    console.log(err)
+    })
+    
+
+
+
+app.listen(port,()=>{
+    console.log(`server running on port : ${port}`)
+})
